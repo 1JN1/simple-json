6a1e5be3c21a37c02a3929ae15f4e48dd774a81f
王文涛 <@LAPTOP-6A69VN6P>
2025-04-16 15:45:30 +0800

    优化了语法解析器

--- src/main/java/com/study/json/JsonParser.java
+++ src/main/java/com/study/json/JsonParser.java
@@ -14,22 +14,16 @@ import java.util.ListIterator;
 public class JsonParser {
 
     /**
-     * 词法解析器生成的token序列
-     */
-    private final List<Token> tokens;
-
-    /**
-     * 词法解析器生成的token序列的迭代器
+     * 当前token的迭代器
      */
     private final ListIterator<Token> iterator;
 
     /**
-     * 当前token
+     * 当前要进行解析的Token
      */
     private Token currentToken;
 
     public JsonParser(List<Token> tokens) {
-        this.tokens = tokens;
         this.iterator = tokens.listIterator();
         if (iterator.hasNext()) {
             this.currentToken = iterator.next();
@@ -37,56 +31,66 @@ public class JsonParser {
     }
 
     public Object parse() {
-        return parseValue();
+
+        Object result = parseValue();
+
+        if (currentToken.getType() != TokenType.END_DOCUMENT) {
+            throw new JsonParseException("Extra data after end of JSON: " + currentToken);
+        }
+
+        return result;
     }
 
     private Object parseValue() {
-        switch (currentToken.getType()) {
-            case BEGIN_OBJECT:
-                return parseObject();
-            case BEGIN_ARRAY:
-                return parseArray();
-            case STRING:
-                return parseString();
-            case NUMBER:
-                return parseNumber();
-            case BOOLEAN:
-                return parseBoolean();
-            case NULL:
-                return parseNull();
-            default:
-                throw new JsonParseException("Unexpected token: " + currentToken);
-        }
+        return switch (currentToken.getType()) {
+            case BEGIN_OBJECT -> parseObject();
+            case BEGIN_ARRAY -> parseArray();
+            case STRING -> parseString();
+            case NUMBER -> parseNumber();
+            case BOOLEAN -> parseBoolean();
+            case NULL -> parseNull();
+            default -> throw new JsonParseException("Unexpected token: " + currentToken);
+        };
     }
 
     private JsonObject parseObject() {
-        JsonObject jsonObject = new JsonObject();
+
+        JsonObject object = new JsonObject();
+
         consume(TokenType.BEGIN_OBJECT);
 
         while (currentToken.getType() != TokenType.END_OBJECT) {
-            String key = parseString();
-            consume(TokenType.SEP_COLON);
-            Object value = parseValue();
-            jsonObject.put(key, value);
 
+            // 解析key
+            String key = parseString();
+
+            // 解析:
+            consume(TokenType.SEP_COLON);
+
+            // 解析value
+            Object value = parseValue();
+            object.put(key, value);
+
+            // 还没有结束，下一个应该是','字符
             if (currentToken.getType() != TokenType.END_OBJECT) {
-                // 还没有结束，下一个应该是，字符
                 consume(TokenType.SEP_COMMA);
             }
+
         }
 
         consume(TokenType.END_OBJECT);
-        return jsonObject;
+
+        return object;
     }
 
     private JsonArray parseArray() {
+
         JsonArray jsonArray = new JsonArray();
         consume(TokenType.BEGIN_ARRAY);
 
         while (currentToken.getType() != TokenType.END_ARRAY) {
             Object value = parseValue();
             jsonArray.add(value);
-
             if (currentToken.getType() != TokenType.END_ARRAY) {
                 consume(TokenType.SEP_COMMA);
             }
@@ -97,18 +101,22 @@ public class JsonParser {
     }
 
     private String parseString() {
+
         String value = currentToken.getValue();
+
         consume(TokenType.STRING);
+
         return value;
     }
 
     private Number parseNumber() {
         String value = currentToken.getValue();
         consume(TokenType.NUMBER);
-
+        // 判断是否是浮点数，如果是，则返回double类型
         if (value.contains(".") || value.contains("e") || value.contains("E")) {
             return Double.parseDouble(value);
         } else {
+            // 尝试使用整形进行解析，不行则使用长整形进行解析
             try {
                 return Integer.parseInt(value);
             } catch (NumberFormatException e) {
@@ -129,15 +137,19 @@ public class JsonParser {
     }
 
     private void consume(TokenType expectedType) {
-        // 当前token类型和期望类型不匹配，抛出异常
+
+        // 当前token的类型不是期望的类型，抛出异常
         if (currentToken.getType() != expectedType) {
             throw new JsonParseException("Expected " + expectedType + " but found " + currentToken);
         }
+
+        // 指向下一个token
         if (iterator.hasNext()) {
             currentToken = iterator.next();
         } else {
-            // 结束标记
+            // 已经解析完Token了，放置结束标记
             currentToken = new Token(TokenType.END_DOCUMENT, null);
         }
     }
+
 }
